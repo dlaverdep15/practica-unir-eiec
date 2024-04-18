53d914805618bcdfb64773b320f8def4266320e3
dlaverdep15 <davidparra1506@hotmail.com>
2024-04-18 04:11:01 +0200

    Se sube una aplicacion hola unir el cual también contiene mi nombre

---
+++ aplicacion_java/holaUnir.java
@@ -0,0 +1,12 @@
+// HolaUnir.java
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprimir "Hola estimada comunidad Unir, esta es la actividad 1 de Estornos de integración continua, los estudiantes son:
+        // primer estudiante: David Laverde
+        // Segundo estudiante:
+        // Tercer estudiante:
+        // Cuarto estudiante:  " en la consola
+        System.out.println("Hola estimada comunidad Unir, esta es la actividad 1 de Estornos de integración continua");
+    }
+}
