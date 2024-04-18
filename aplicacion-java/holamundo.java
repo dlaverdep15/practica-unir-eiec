ece2de71709984c2b297f02fdbd8ac7402781eba
dlaverdep15 <davidparra1506@hotmail.com>
2024-04-18 03:55:11 +0200

    Se sube un hola unir en JAVA, con mi nombre

---
+++ aplicacion-java/holamundo.java
@@ -0,0 +1,12 @@
+// HolaMundo.java
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprimir "Hola estimada comunidad Unir, esta es la actividad 1 de Estornos de integración continua, los estudiantes son: 
+	// primer estudiante: David Laverde
+	// Segundo estudiante: 
+	// Tercer estudiante: 
+	// Cuarto estudiante:  " en la consola
+        System.out.println("Hola Mundo");
+    }
+}
