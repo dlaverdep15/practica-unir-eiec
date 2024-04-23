426c986b349ba99d11ac78c23bb3ec1ed6c0a58c
joel2064 <joel2064@gmail.com>
2024-04-22 20:39:36 -0500

    cambios de joel

---
+++ aplicacion_java/HolaMundo.java
@@ -0,0 +1,13 @@
+// HolaUnir.java
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        // Imprimir "Hola estimada comunidad Unir, esta es la actividad 1 de Estornos de integración continua, los estudiantes son:
+        // primer estudiante: David Laverde
+        // Segundo estudiante:
+        // Tercer estudiante:
+        // Cuarto estudiante:  " en la consola
+        //Correccion de menaje de salida - joel
+        System.out.println("Hola estimada comunidad Unir, esta es la actividad 1 de Entornos de integración continua");
+    }
+}
